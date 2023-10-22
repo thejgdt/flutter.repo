e2afc7a1ee346d2bf1f108cfbc5cb58fffeae3a3
ctva <theaberchio@gmail.com>
2023-10-22 15:12:33 +0700

    AboutPage(): implement `share_plus` & `url_launcher`

--- lib/page/about_page.dart
+++ lib/page/about_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatefulWidget {
-  const AboutPage({super.key});
+  const AboutPage({Key? key}) : super(key: key);
 
   @override
   State<AboutPage> createState() => _AboutPageState();
@@ -22,39 +24,84 @@ class _AboutPageState extends State<AboutPage> {
       body: Column(
         children: [
           Expanded(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Align(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Align(
                       alignment: Alignment.center,
-                      child: Icon(Icons.qr_code_scanner,
-                          size: 100, color: Colors.blue.shade900)),
-                  Align(
+                      child: Icon(
+                        Icons.qr_code_scanner,
+                        size: 100,
+                        color: Colors.blue.shade900,
+                      ),
+                    ),
+                    Align(
                       alignment: Alignment.center,
                       child: Text(
                         "QR Code Scanner App",
                         style: TextStyle(
-                            color: Colors.blue.shade900, fontSize: 16),
-                      )),
-                  const Text(
-                    "Aplikasi Scan QR Code ini adalah solusi yang cepat, efisien dan mudah digunakan untuk membaca dan mengolah informasi dari QR Code. Dengan antarmuka yang sederhana, aplikasi ini memungkinkan pengguna untuk dengan cepat memindai dan mendapatkan data yang tersembunyi dalam kode QR.",
-                    textAlign: TextAlign.center,
-                  ),
-                  const Text(
-                    "Adapun aplikasi ini masih dalam tahap pengembangan, jadi mohon maklumi segala kekurangannya.",
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              )
-            ],
-          )),
+                          color: Colors.blue.shade900,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      "Aplikasi Scan QR Code ini adalah solusi yang cepat, efisien dan mudah digunakan untuk membaca dan mengolah informasi dari QR Code. Dengan antarmuka yang sederhana, aplikasi ini memungkinkan pengguna untuk dengan cepat memindai dan mendapatkan data yang tersembunyi dalam kode QR. \n"
+                      "Adapun aplikasi ini masih dalam tahap pengembangan, jadi mohon maklumi segala kekurangannya.",
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            openLink('https://github.com/thejgdt');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                          ),
+                          child: const Text(
+                            "GitHub",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            openLink('https://gitlab.com/thejgdt');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.orange,
+                          ),
+                          child: const Text(
+                            "GitLab",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Share.share("thejgdt here!");
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                          ),
+                          child: const Text(
+                            "Share",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
           const Text(
             "Version 1.0.0",
             style: TextStyle(fontWeight: FontWeight.bold),
@@ -62,5 +109,13 @@ class _AboutPageState extends State<AboutPage> {
         ],
       ),
     );
+  }
+
+  Future<void> openLink(String url) async {
+    if (await launchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw "Sorry lur ga bisa ngebukain $url";
+    }
   }
 }
